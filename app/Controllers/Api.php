3f7a509e79a17ecae0084c701a5112d32b47c61b
Frederick-dev23 <79848521+Frederick-dev23@users.noreply.github.com>
2021-03-11 11:45:44 -0400

    recherche api ok

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -5,13 +5,36 @@ use App\Models\ContactsModel;
 
 class Api extends BaseController
 {
+    public $contact = null;
+    public function __construct(){
+        $this->contact = new ContactsModel();
+    }
     /*********** List , modify and add  */    
 	public function index()
 	{
-        $contact = new ContactsModel();
-        $listContact = $contact->orderBy('last_Name','ASC')->orderBy('first_Name','ASC')->paginate(6);
-        echo json_encode($listContact);
-		
+        $typeSearch = $this->request->getvar('typeSearch');
+        $elementSearch = $this->request->getvar('element');
+        // $listContact = $this->contact->orderBy('last_Name','ASC')->orderBy('first_Name','ASC')->paginate(6);
+        
+         if(!empty($typeSearch) && !empty($elementSearch)){
+		// 	//dd($this->request->getvar('search'));
+		 	switch($typeSearch){
+				
+		 		case "recherche" :
+					$searchContact = $this->contact
+					->like('last_Name',$elementSearch,'both',null,true)
+					->orlike('first_name',$elementSearch,'both',null,true)
+					->orderBy('last_Name','ASC')
+                    ->orderBy('first_Name','ASC')
+                    ->paginate(6);
+
+		 			break;
+
+		 		default;
+             }    
+
+         return $this->response->setJSON($searchContact);
+         }    
 	}
     
     
