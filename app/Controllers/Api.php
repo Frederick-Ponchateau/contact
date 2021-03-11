484b7d2c9e39c65922eb80227b0c913efbff2ed9
Frederick-dev23 <79848521+Frederick-dev23@users.noreply.github.com>
2021-03-11 15:14:21 -0400

    delete ok

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -12,8 +12,10 @@ class Api extends BaseController
     /*********** List , modify and add  */    
 	public function index()
 	{
+        /************** Recupere les données en Post ********************/
         $typeSearch = $this->request->getvar('typeSearch');
         $elementSearch = $this->request->getvar('element');
+        $reponse = ['response' => false];
         // $listContact = $this->contact->orderBy('last_Name','ASC')->orderBy('first_Name','ASC')->paginate(6);
         
          if(!empty($typeSearch) && !empty($elementSearch)){
@@ -27,21 +29,40 @@ class Api extends BaseController
 					->orderBy('last_Name','ASC')
                     ->orderBy('first_Name','ASC')
                     ->paginate(6);
-
+                    
 		 			break;
 
 		 		default;
-             }    
 
-         return $this->response->setJSON($searchContact);
-         }    
+            }    
+        } 
+        if($searchContact){
+            $reponse = ['response' => true];
+            return $this->response->setJSON($searchContact);
+        }
+        return $this->response->setJSON($reponse);   
 	}
     
     
     /******* Supprime / Suppression multiple / les id en parama **********/
     public function delete()
     {
-        
+        /************* 1-Je recupere l'id du contact  supprimé
+         * *********** 2-Si il existe je passe au 3
+         * ************ 3-Je fais ma requette pour supp
+         *************** 4-l'etat de la suppression 
+         ******************************************************/
+        /*** 1 ***/
+        $idContact = $this->request->getvar('id');
+        $delete = ['response' => false];
+        /*** 2 ***/
+        if($idContact){
+        /*** 3 ***/
+        $this->contact->where('id', $idContact)->delete();
+        /*** 4 ***/
+        $delete = ['response' => true];
+        }
+        return $this->response->setJSON($delete);
     }
     /************ Modifier - Ajouter ***********/
     public function edit()
@@ -51,7 +72,28 @@ class Api extends BaseController
     /********* Favory param id du contact a supp *********/
     public function favory()
     {
-        
+        /***************************************************************************
+        ****** 1- je recupere l'id **********
+         ****** 2- je verifie si il exisite si oui ******
+         ****** 3- Je je fait ma requete pour verifier si il est favory et je modifie ********
+         ****** 4- j'affiche si oui et je modifie  pareil pour non  *********
+        ****************************************************************************/
+        /*** 1 ***/
+        $id = $this->request->getvar('id');
+        /*** 2 ***/
+        if($id){
+        /*** 3 ***/
+            $favory = $this->contact->where('id',$id)->first();
+            $fav = [ 'reponse' => $favory["favory"]];
+            if($fav['reponse'] == 'No')
+            {
+                // $this->contact->where('id',$id)
+                //         ->set()
+                //         ->update();
+                return $this->response->setJSON($favory["favory"]);
+            }
+            return $this->response->setJSON($fav);
+        }
     }
     
 
